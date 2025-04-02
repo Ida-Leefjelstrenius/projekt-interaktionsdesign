173d2749f601c120b692d4e312893cdcbed00985
JasleenBharaj <ja5581bh-s@student.lu.se>
2025-04-02 17:04:12 +0200

    Changed home page and added settings, inventory and profile pages

--- app/src/main/java/com/example/projektinteraktionsdesign/InventoryPage.java
+++ app/src/main/java/com/example/projektinteraktionsdesign/InventoryPage.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SettingsPage extends AppCompatActivity {
+public class InventoryPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/projektinteraktionsdesign/ProfilePage.java
+++ app/src/main/java/com/example/projektinteraktionsdesign/ProfilePage.java
@@ -1,4 +1,32 @@
 package com.example.projektinteraktionsdesign;
 
-public class ProfilePage {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.profilepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String h = "This app is developed as a part of the Hello Sensor project in the advanced interaction design course.";
+
+
+    }
+
+    public String text() {
+        return "hej";
+    }
 }
