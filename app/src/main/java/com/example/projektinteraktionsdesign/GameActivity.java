f0169c6431604f8f218dc174df7b575fd7eb0c9f
Willard Råborg <wi5406ra-s@student.lu.se>
2025-04-09 14:22:27 +0200

    fixed time tracking

--- app/src/main/java/com/example/projektinteraktionsdesign/GameActivity.java
+++ app/src/main/java/com/example/projektinteraktionsdesign/GameActivity.java
@@ -1,19 +1,24 @@
 package com.example.projektinteraktionsdesign;
 
+import android.graphics.Color;
 import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
     GameView gameView;
     ImageView player;
+    TextView timer;
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
@@ -29,12 +34,32 @@ public class GameActivity extends AppCompatActivity {
         rootLayout.addView(gameView);
 
         createPlayer(rootLayout); //Skapa spelaren
-
+        timer = new TextView(this);
+        timer.setTextSize(25);
+        timer.setTextColor(Color.WHITE);
+        timer.setPadding(20,20,20,20);
         gameView.setPlayer(player);
+        rootLayout.addView(timer);
         setContentView(rootLayout);
-
-
+        long startTime;
+        Handler timerHandler = new Handler();
+        Runnable timerRunnable;
         new TiltSensor(this, gameView, player); //Skapa tilt sensorn
+        startTime = System.currentTimeMillis();
+
+        timerRunnable = new Runnable() {
+            @Override
+            public void run() {
+                long millis = System.currentTimeMillis() - startTime;
+                int seconds = (int) (millis / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+
+                timer.setText("Time alive: " + String.format("%d:%02d", minutes, seconds));
+                timerHandler.postDelayed(this, 500);
+            }
+        };
+        timerHandler.post(timerRunnable);
     }
 
     private int dpToPx(int dp) {
